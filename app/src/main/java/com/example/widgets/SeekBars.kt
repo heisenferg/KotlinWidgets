6d866d478712cbc965f7ee072ea7388edba2a5f8
heisenferg <heisenferg@gmail.com>
2022-07-08 13:50:03 +0200

    Final seccion 10

--- app/src/main/java/com/example/widgets/SeekBars.kt
+++ app/src/main/java/com/example/widgets/SeekBars.kt
@@ -1,11 +1,66 @@
 package com.example.widgets
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 
 class SeekBars : AppCompatActivity() {
+
+    private lateinit var myontext: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seek_bars)
+
+        myontext = this
+
+        var sbCustom: SeekBar = findViewById(R.id.sbNormal)
+
+        sbCustom.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser){
+                    Toast.makeText(myontext, "Lo cambiastge tú", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+        } )
+
+        GlobalScope.launch {
+            changeSeekBar(sbCustom)
+        }
+
+
+        /**
+         * RatingBar
+         */
+        var ratingBar: RatingBar = findViewById(R.id.ratingBar)
+        var textRating: TextView = findViewById(R.id.textViewRating)
+        ratingBar.rating = 1f
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            textRating.text = "Puntuación: ${rating}/${ratingBar.numStars}"
+        }
+
+    }
+
+    private fun changeSeekBar(sb: SeekBar){
+        while(true) {
+            sleep(100L)
+            sb.incrementProgressBy(5)
+        }
     }
 }
---
+++ app/src/main/java/com/example/widgets/searchView.kt
@@ -0,0 +1,38 @@
+package com.example.widgets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import androidx.core.view.contains
+
+class searchView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_view)
+
+
+        var svUsuarios: SearchView = findViewById(R.id.searchView)
+        var lvUsuarios: ListView = findViewById(R.id.lvOptions)
+
+        val users = arrayOf("Paco", "Fernanndo", "Ramón", "Aurelio", "Roberto", "Rigoberta", "Lucía", "María", "Isabel")
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+
+        lvUsuarios.adapter = adapter
+
+        svUsuarios.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                svUsuarios.clearFocus()
+                if (users.contains(query)) adapter.filter.filter(query)
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                adapter.filter.filter(query)
+                return false
+            }
+
+        })
+    }
+}
